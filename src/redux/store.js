8cc3f47479ef809a9f7d7e38f4626afff767c445
Elena Semeshyna <81982732+LenaXmi@users.noreply.github.com>
2022-01-14 12:59:34 +0100

    add persistStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,20 @@
 import { createStore, combineReducers } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { composeWithDevTools } from "redux-devtools-extension";
 import phonebookReducer from "./phonebook/phonebook-reducers";
+
+const persistConfig = {
+  key: "contacts",
+  storage,
+};
 
 const rootReducer = combineReducers({
   phonebook: phonebookReducer,
 });
 
-const store = createStore(rootReducer, composeWithDevTools());
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export default store;
+const store = createStore(persistedReducer, composeWithDevTools());
+const persistor = persistStore(store);
+export default { store, persistor };
